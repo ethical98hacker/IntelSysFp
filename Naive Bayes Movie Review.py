8655ae01593a97b1656635c2499938e9c875767a
ethical98hacker <ethicalhacker123789@gmail.com>
2019-01-05 18:50:05 +0700

    adding ngram word

--- Naive Bayes Movie Review.py
+++ Naive Bayes Movie Review.py
@@ -6,23 +6,30 @@ from nltk.corpus import stopwords
 from sklearn import naive_bayes
 from sklearn.feature_extraction.text import TfidfVectorizer
 import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split, cross_val_score, StratifiedKFold
 
 # Sentiment -> positive = 1; negative = 0
 # to separate the class and the features
-from sklearn.model_selection import train_test_split, cross_val_score, StratifiedKFold
-
 train_dataset = pd.read_csv("data/imdb_train.txt", names=['sentiment'])
 predict_dataset = pd.read_csv("data/imdb_test.txt", names=['txt'])
 train_dataset[['sentiment', 'txt']] = train_dataset["sentiment"].str.split(" ", 1, expand=True)
 train_dataset["txt"] = train_dataset["txt"]
 
+# Showing the data of positive and negative review distribution are equal
+train_dataset.groupby('sentiment').txt.count().plot.bar(ylim=0)
+plt.show()
+
 # To remove the common word and count the probability of each word while also doing smoothing
 stopset = set(stopwords.words('english'))
-vectorizer = TfidfVectorizer(use_idf=True, smooth_idf=True, lowercase=True, strip_accents='ascii', stop_words=stopset)
+vectorizer = TfidfVectorizer(use_idf=True, smooth_idf=True, lowercase=True, strip_accents='ascii', stop_words=stopset,
+                             ngram_range=(1, 2))
 
+# convert train data using vectorizer
 x = vectorizer.fit_transform(train_dataset.txt)
 y = train_dataset.sentiment
 
+# convert data to be predict
 # x_predict = vectorizer.transform(predict_dataset)
 # print(x_predict)
 
@@ -38,8 +45,11 @@ kfold = StratifiedKFold(n_splits=2, shuffle=True)
 model_cv_score = cross_val_score(estimator=clf, X=x, y=y, cv=kfold, verbose=True)
 print("Results: %.2f% % (%.2f% %)" % (model_cv_score.mean() * 100, model_cv_score.std() * 100))
 
+score = clf.score(x_test, y_test)
+print("Score :", score)
+
 movie_reviews = np.array(["it was a terrible movie",
-                          "this is a nice movie",
+                          "something is a nice movie",
                           "check out this movie, it is a great movie.. worth to watch"])
 
 movie_review_vector = vectorizer.transform(movie_reviews)
